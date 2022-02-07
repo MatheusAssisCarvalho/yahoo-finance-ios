99e48e944aa6bc5d2d7153229e5fc9bb04c80f7d
Pedro Galdiano <pedrocastro014@gmail.com>
2022-02-07 10:20:29 -0300

    iniciando o projeto

---
+++ yahoo-finance-ios/yahoo-finance-ios/ViewModel/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  yahoo-finance-ios
+//
+//  Created by PEDRO GALDIANO DE CASTRO on 07/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBlue
+    }
+
+}
